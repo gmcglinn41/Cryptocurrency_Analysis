7265a3f90b7b5390bd18484e238aeae5622e982e
Move-7-8 <69103213+Move-7-8@users.noreply.github.com>
2021-01-21 14:07:15 +0800

    flask finished

--- app.py
+++ app.py
@@ -18,89 +18,60 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-table_overview = Base.classes.asset_overview
-#table_market_symbols = Base.classes.market_symbols
-#table_exchanges = Base.classes.exchanges
-#table_historic_trades = Base.classes.historic_trades
-
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-
 #################################################
 # Flask Routes
 #################################################
-@app.route("/api/demo")
-def demo():
-    dbConnect = engine.connect()
-    df = pd.read_sql('select * from asset_overview', dbConnect).head(20)
-    res = df.to_json(orient='records')
-    dbConnect.close()
-    return res
 
 @app.route("/")
-def overview():
+def index():
 
     session = Session(engine)
-
-
     session.close()
 
     return render_template('index.html')
 
-## Use HTML links to the other pages instead of the return of apis in routes
+## ^^ Use HTML links to the other pages instead of the return of apis in routes (?)
+
+@app.route("/api/overview")
+def overview():
+    dbConnect = engine.connect()
+    df = pd.read_sql('select * from asset_overview', dbConnect).head(20)
+    overview_result = df.to_json(orient='records')
+    dbConnect.close()
+    return overview_result
 
 
-@app.route("/api/v1.0/overview")
-def change():
+@app.route("/api/symbols")
+def symbols():
 
-    session = Session(engine)
+    dbConnect = engine.connect()
+    df = pd.read_sql('select * from market_symbols', dbConnect).head(20)
+    symbols_result = df.to_json(orient='records')
+    dbConnect.close()
+    return symbols_result
 
-    asset_id = session.query(table_overview.asset_id).all()
-    name = session.query(table_overview.name).all()
-    price_usd = session.query(table_overview.price_usd).all()
-
-
-    # historic_trades = session.query(table_historic_trades).all()
-    # price = session.query(table_historic_trades.price).all()
-    # size = session.query(table_historic_trades.size).all()
-
-    session.close()
-
-    return jsonify(asset_id, name, price_usd)
-    # render_template('changes.html', change=change)
-
-@app.route("/api/v1.0/exchanges")
+@app.route("/api/exchanges")
 def exchange():
 
-    session = Session(engine)
+    dbConnect = engine.connect()
+    df = pd.read_sql('select * from exchanges', dbConnect).head(20)
+    exchange_result = df.to_json(orient='records')
+    dbConnect.close()
+    return exchange_result
 
-    exchange_id = session.query(table_exchanges).all()
-    website = session.query(table_exchanges).all()
-    name = session.query(table_exchanges).all()
-    data_symbols_count = session.query(table_exchanges).all()
-    volume_1mth_usd = session.query(table_exchanges).all()
+@app.route("/api/historic_trades")
+def historic():
 
-    session.close()  
-
-    return jsonify(exchange_id, website, name, data_symbols_count, volume)
-
-
-# ## Only doing coin family if we have time 
-
-# # @app.route("/api/v1.0/coin_family")
-# # def coinfamily():
-
-# #     session = Session(engine)
-
-
-
-# #     session.close()
-
-# #     return render_template('coin_family.html', coin_fam=coin_fam)
+    dbConnect = engine.connect()
+    df = pd.read_sql('select * from historic_trades', dbConnect).head(20)
+    historic_result = df.to_json(orient='records')
+    dbConnect.close()
+    return historic_result
 
 
 if __name__ == '__main__':
