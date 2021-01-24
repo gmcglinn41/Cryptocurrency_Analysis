e2246b2c68c4cb11692e3696f2641509cf99bd2e
JB-DA <70473223+JB-DA@users.noreply.github.com>
2021-01-24 12:50:16 +0800

    restructure

--- app.py
+++ app.py
@@ -9,72 +9,94 @@ from flask import Flask, jsonify, render_template, url_for, redirect
 import json
 import jinja2
 
-#################################################
-# Database Setup
-#################################################
+##### DATABASE SETUP
+###
+#
 engine = create_engine(f"postgresql://postgres:postgres@localhost:5432/crypto_analysis_db")
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+# Reflect the tables
 Base.prepare(engine, reflect=True)
 
-#################################################
-# Flask Setup
-#################################################
+##### FLASK SETUP
+###
+#
 app = Flask(__name__)
 
-#################################################
-# Flask Routes
-#################################################
+##### ROUTES
+###
+#
 
-@app.route("/")
-def index():
-    #df = pd.read_csv('/static/data.csv').drop('Open', axis=1)
-    df = pd.read_csv('data.csv')
-    chart_data = df.to_dict(orient='records')
-    chart_data = json.dumps(chart_data, indent=2)
-    data = {'chart_data': chart_data}
-    return data
+# API Pages
+@app.route("/api/overview")
+def overview():
+    dbConnect = engine.connect()
+    df = pd.read_sql('select * from asset_overview', dbConnect).head(20)
+    results = df.to_json(orient='records')
+    json_overview = json.loads(results)
+    #json.dumps(parsed)
+    dbConnect.close()
+    return jsonify(json_overview)
 
-## ^^ Use HTML links to the other pages instead of the return of apis in routes (?)
+    # dump_data = dumps(data)
+    # json_data = json.loads(dump_data)
+    # return jsonify(json_data) 
+    # dump_latest = dumps(latest) 
+    # json_latest = json.loads(dump_latest)
+    # return jsonify(json_latest)
 
-# @app.route("/api/overview")
-# def overview():
+# @app.route("/api/")
+# def index():
 #     dbConnect = engine.connect()
 #     df = pd.read_sql('select * from asset_overview', dbConnect).head(20)
-#     overview_result = df.to_json(orient='records')
+#     results_index = df.to_json(orient='records')
 #     dbConnect.close()
-#     return overview_result
+#     return (results_index)
 
-
-# @app.route("/api/team")
-# def symbols():
-
+# @app.route("/api/overview")
+# def index():
 #     dbConnect = engine.connect()
-#     df = pd.read_sql('select * from market_symbols', dbConnect).head(20)
-#     symbols_result = df.to_json(orient='records')
+#     df = pd.read_sql('select * from asset_overview', dbConnect).head(20)
+#     results_index = df.to_json(orient='records')
 #     dbConnect.close()
-#     return render_template('OurTeam.html')
+#     return (results_index)
 
-# @app.route("/api/exchanges")
-# def exchange():
-
+# @app.route("/api/overview")
+# def index():
 #     dbConnect = engine.connect()
-#     df = pd.read_sql('select * from exchanges', dbConnect).head(20)
-#     exchange_result = df.to_json(orient='records')
+#     df = pd.read_sql('select * from asset_overview', dbConnect).head(20)
+#     results_index = df.to_json(orient='records')
 #     dbConnect.close()
-#     return exchange_result
+#     return (results_index)
 
-# @app.route("/api/historic_trades")
-# def historic():
+# HTML PAGES
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-#     dbConnect = engine.connect()
-#     df = pd.read_sql('select * from historic_trades', dbConnect).head(20)
-#     historic_result = df.to_json(orient='records')
-#     dbConnect.close()
-#     return historic_result
+# @app.route('/coins')
+# def coins():
+#     return render_template('coins.html')
 
+# @app.route('/markets')
+# def markets():
+#     return render_template('markets.html')
 
+# @app.route('/data')
+# def data():
+#     return render_template('data.html')
+
+# @app.route('/team')
+# def team():
+#     return render_template('team.html')
+
+# @app.route('/api')
+# def api():
+#     return render_template('api.html')
+
+##### RUN THAT CODE
+###
+#
 if __name__ == '__main__':
     app.run(debug=True)
