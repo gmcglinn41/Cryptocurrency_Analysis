93aab35b6685562c225ba737c0cb1ac75820a2bd
JB-DA <70473223+JB-DA@users.noreply.github.com>
2021-01-28 21:08:35 +0800

    coin graph

--- static/js/coins.js
+++ static/js/coins.js
@@ -1,124 +1,78 @@
-//alert("The Coins Javascript file was loaded...");
-var plotpoints = {};
-let chartURL = "/api/api_coins";
+// var coinlist = {};
+// let chartURL = "/api/api_coins";
 
-//Define chart size
-var svgWidth = 1200;
-var svgHeight = 660;
+// function initdropdown() {
 
-//Define chart margins
-var chartMargin = {
-  top: 30,
-  right: 100,
-  bottom: 30,
-  left: 100
-};
+//   var dropdown  = d3.select("#selDataset");
 
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+//   d3.json(chartURL).then((d)=> {
+//     d.forEach(function(id, index) {
+//       //console.log(d[index].asset_id)
+//       //console.log(id.asset_id)
+//       dropdown.append("option").text(id.asset_id).property("value", index);
+//     });
+//   });
+// };
 
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3.select(".mainchart")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+// initdropdown();
 
-// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+// // Initializes the page with a default plot
+// function init() {
+//     data = [{
+//       x: [1, 2, 3, 4, 5],
+//       y: [1, 2, 4, 8, 16] }];
 
-// Import Data
-d3.json(chartURL).then(function(chartData, err) {
-  if (err) throw err;
+//     Plotly.newPlot("mainchartdiv", data);
+//   }
 
-  chartData.shift();
-  console.log(chartData)
+//   init();
 
+//   // Call updatePlotly() when a change takes place to the DOM
+//   d3.selectAll("#selDataset").on("change", updatePlotly);
 
-  // load data
-  chartData.forEach(function(d) {
-    plotpoints.asset_id = +d.asset_id;
-    plotpoints.price_usd = +d.price_usd;
-    console.log(d.asset_id + " --- " + d.price_usd);
-  });
+//   // This function is called when a dropdown menu item is selected
+//   function updatePlotly() {
+//     // Use D3 to select the dropdown menu
+//     var dropdownMenu = d3.select("#selDataset");
+//     // Assign the value of the dropdown menu option to a variable
+//     var dataset = dropdownMenu.property("value");
+//     console.log(dataset);
+//     // Initialize x and y arrays
+//     var x = [];
+//     var y = [];
 
-    // Step 2: Create scale functions
-    // ==============================
-    var xBandScale = d3.scaleBand()
-    .domain(chartData.map(plotpoints => plotpoints.asset_id))
-    .range([0, chartWidth])
-    .padding(0.1);
+//     if (dataset === 'dataset1') {
+//       x = [1, 2, 3, 4, 5];
+//       y = [1, 2, 4, 8, 16];
+//     }
 
-  // Create a linear scale for the vertical axis.
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(chartData, plotpoints => plotpoints.price_usd)])
-    .range([chartHeight, 0]);
+//     if (dataset === 'dataset2') {
+//       x = [10, 20, 30, 40, 50];
+//       y = [1, 10, 100, 1000, 10000];
+//     }
 
-    // Step 3: Create axis functions
-    // ==============================
-    var bottomAxis = d3.axisBottom(xBandScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-
-    // Step 4: Append Axes to the chart
-    // ==============================
-    chartGroup.append("g")
-    .call(leftAxis);
-
-    chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
-
-    // // Bar
-    var barGroup = chartGroup.selectAll(".bar")
-    .data(chartData)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", plotpoints => xBandScale(plotpoints.asset_id))
-    .attr("y", plotpoints => yLinearScale(plotpoints.price_usd))
-    .attr("width", xBandScale.bandwidth())
-    .attr("height", plotpoints => chartHeight - yLinearScale(plotpoints.price_usd));
-
-    // Step 6: Initialize tool tip
-    // ==============================
-    var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (`Name: ${d.name}<br>Asset Code: ${d.asset_id}<br>Price in USD: ${d.price_usd}`);
-      });
-
-    // Step 7: Create tooltip in the chart
-    // ==============================
-    chartGroup.call(toolTip);
-
-    // Step 8: Create event listeners to display and hide the tooltip
-    // ==============================
-    barGroup.on("mouseover", function(data) {
-      toolTip.show(data, this);
-    })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
-
-    // Create axes labels
-    chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - chartMargin.left + 40)
-      .attr("x", 0 - (chartHeight / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Price (USD)");
-
-    chartGroup.append("text")
-      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
-      .attr("class", "axisText")
-      //.attr("transform", "rotate(-65)")
-      .text("Cryptocurrency Code");
+//     // Note the extra brackets around 'x' and 'y'
+//     Plotly.restyle("mainchartdiv", "x", [x]);
+//     Plotly.restyle("mainchartdiv", "y", [y]);
+//   }
 
 
-  }).catch(function(error) {
-    console.log(error);
-  });
+
+
+//COIN UPDATE FUNCTION USING JAVASCRIPT
+var myvar= {};
+var yplots = [];
+
+$.ajax({
+  url: 'https://rest.coinapi.io/v1/ohlcv/BTC/USD/history?period_id=6HRS&time_start=2021-01-26T00:00:00', // The URL to the API
+  type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+  data: {}, // Additional parameters here
+  dataType: 'json',
+  //success: function(data) { myvar = data; console.dir((data.source)); },
+  //success: function (data) { myvar = data; console.log(data); },
+  success: function(data) { myvar = data; myvar.forEach(function (item, index) {console.log( data[index].price_close );});                },
+  error: function (err) { alert(err); },
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader("X-CoinAPI-Key", "1830D89F-A633-4F73-9707-3A7FAFE5C0F0"); // Enter here your key
+  }
+}); 
